0cf9d82c1b7f6288870ea5f9a6de7b3fff6b3f74
Julius Hell <jhell2306@googlemail.com>
2023-03-08 19:35:15 +0100

    Added ping route

--- src/server.js
+++ src/server.js
@@ -7,6 +7,11 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// eslint-disable-next-line no-unused-vars
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
 app.use('/message', messageController);
 
 app.listen(4000, '0.0.0.0', () => {
