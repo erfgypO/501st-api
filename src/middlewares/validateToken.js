0eead8f89dc2264ddd2a78c5703fbdaffc349646
Julius Hell <jhell2306@googlemail.com>
2023-03-09 20:30:55 +0100

    Fixed env var name - pt.2

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -10,7 +10,7 @@ export default function validateToken(req, res, next) {
   const token = authorization.split(' ')[1];
 
   try {
-    jwt.verify(token, process.env.PASSWORD);
+    jwt.verify(token, process.env.JWT_TOKEN);
     next();
   } catch (e) {
     res.sendStatus(401);
